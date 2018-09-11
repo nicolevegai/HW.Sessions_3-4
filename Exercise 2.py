0f87de83ef8ae2fb103a19df9b1d1ba613ea619b
nicolevegai <nicol-v-8@hotmail.com>
2018-09-11 23:16:59 +0200

    HW1

---
+++ Exercise 2.py
@@ -0,0 +1,7 @@
+
+num1 = int (input ('Enter the first number: '))
+num2 = int (input ('Enter the second number: '))
+
+div = int ((num1 - 1) // num2) +1
+
+print('the rounded up division result is', div)
